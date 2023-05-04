a42b4458c186b17811e2224b5f5d55358ba6d337
Gautham <gauthamskaniyattil@gmail.com>
2023-05-04 21:40:27 +0530

    Added the no value scenerio

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 'use strict';
 
-console.log(document.querySelector('.label-score').textContent);
+// console.log(document.querySelector('.label-score').textContent);
+document.querySelector('.check').addEventListener('click', function () {
+  let guess = document.querySelector('.guess').value;
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔No Number Entered';
+  }
+});
