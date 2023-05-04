99c4b183373665c1350b9b5f035584ff83bf5907
Gautham <gauthamskaniyattil@gmail.com>
2023-05-04 22:09:04 +0530

    added scoring mechanics

--- script.js
+++ script.js
@@ -5,6 +5,13 @@
 const randomNumber = Math.trunc(Math.random() * 20 + 1);
 document.querySelector('.number').textContent = randomNumber;
 
+let score = 20;
+
+let changeScore = function () {
+  score--;
+  document.querySelector('.score').textContent = score;
+};
+
 document.querySelector('.check').addEventListener('click', function () {
   let guess = Number(document.querySelector('.guess').value);
 
@@ -14,7 +21,9 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = '🎉 Correct Number';
   } else if (guess > randomNumber) {
     document.querySelector('.message').textContent = '📈 Too High!';
+    changeScore();
   } else if (guess < randomNumber) {
     document.querySelector('.message').textContent = '📉 Too Low!';
+    changeScore();
   }
 });
